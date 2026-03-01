41f94d4038ba65832af8a0424200dca6973a89c4
Aris Ripandi <riipandi@gmail.com>
2026-03-02 00:23:26 +0700

    fix: router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,7 @@
 import { createRouter } from '@tanstack/react-router'
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
 import * as TanstackQuery from './integrations/tanstack-query/root-provider'
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from './routes.gen'
 
 // Create a new router instance
 export const getRouter = () => {
