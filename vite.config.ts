8cd221c27a05fb1fd83b7d208b1147aff242220f
Aris Ripandi <riipandi@gmail.com>
2026-03-02 00:21:53 +0700

    feat: initial setup Storybook

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,46 @@ import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { nitro } from 'nitro/vite'
-import { fileURLToPath, URL } from 'url'
+import { nitro, type NitroPluginConfig } from 'nitro/vite'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-const config = defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url))
+const isProduction = process.env.NODE_ENV === 'production'
+
+const nitroConfig: NitroPluginConfig = {
+  compatibilityDate: '2025-09-15',
+  preset: 'node_server'
+}
+
+export default defineConfig({
   plugins: [
-    devtools(),
-    nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json']
-    }),
+    devtools({ removeDevtoolsOnBuild: true }),
+    nitro(nitroConfig),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      router: {
+        generatedRouteTree: resolve('./src/routes.gen.ts')
+      }
+    }),
     viteReact({
       babel: {
         plugins: ['babel-plugin-react-compiler']
       }
+    }),
+    tsconfigPaths({
+      projects: [resolve('./tsconfig.json')],
+      ignoreConfigErrors: true
     })
-  ]
+  ],
+  build: {
+    chunkSizeWarningLimit: 1024 * 4,
+    minify: isProduction ? 'esbuild' : false
+  },
+  resolve: {
+    alias: {
+      '@': resolve('./src')
+    }
+  }
 })
-
-export default config
