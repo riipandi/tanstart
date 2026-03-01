17f4ad29bd074617cd281ec0dac4ebc8197b444b
Aris Ripandi <riipandi@gmail.com>
2026-03-02 02:35:48 +0700

    fea: customize generated ID for better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,14 @@
+/**
+ * Better Auth configuration.
+ *
+ * @see: https://better-auth.com/docs/concepts/database#programmatic-migrations
+ * @see: https://better-auth.com/docs/concepts/database#extending-core-schema
+ */
+
 import { betterAuth } from 'better-auth'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { typeid } from 'typeid-js'
+import pkg from '~/package.json' with { type: 'json' }
 import { db } from './db'
 
 export const auth = betterAuth({
@@ -24,6 +33,30 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }
+  },
+  advanced: {
+    cookiePrefix: pkg.name,
+    useSecureCookies: true,
+    crossSubDomainCookies: {
+      enabled: process.env.NODE_ENV === 'production',
+      domain: process.env.APP_DOMAIN
+    },
+    database: {
+      generateId: (opts) => {
+        // Let database auto-generate for specific models
+        if (opts.model === 'user') {
+          return typeid('user').toString()
+        }
+        if (opts.model === 'account') {
+          return typeid('acc').toString()
+        }
+        if (opts.model === 'session') {
+          return typeid('sess').toString()
+        }
+        // Generate UUIDs for other tables
+        return typeid().toUUID()
+      }
     }
   },
   plugins: [tanstackStartCookies()]
