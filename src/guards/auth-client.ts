f5b73f60f7ab4211f5ee2f49347d9e8ec79784cc
Aris Ripandi <riipandi@gmail.com>
2026-03-02 09:03:10 +0700

    feat: add auth with social provider

--- src/guards/auth-client.ts
+++ src/guards/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from 'better-auth/react'
+import { publicEnv } from '#/config'
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+  baseURL: publicEnv.PUBLIC_BASE_URL
+})
 
 export type Session = typeof authClient.$Infer.Session
